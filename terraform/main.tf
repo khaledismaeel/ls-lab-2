125172f069a93cb14905d9b05a77c6b2fbb56c38
khaledismaeel <k.ismaeel@innopolis.university>
2023-02-07 19:57:12 +0300

    Update terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,14 +7,13 @@ terraform {
   }
 }
 
-
-
 resource "yandex_compute_instance" "vm-1" {
   name = "ls-lab-2-instance"
 
   resources {
     cores         = 2
     memory        = 2
+    core_fraction = 20
   }
 
   boot_disk {
@@ -29,8 +28,10 @@ resource "yandex_compute_instance" "vm-1" {
   }
 
   metadata = {
-    ssh-keys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOch1nhLIPC0VVrwjgeN2gPEdLrImi+TD5hqDi+RZAHh khaledismaeel@yandex.ru"
+    user-data = "${file("./cloud-config.yaml")}"
   }
+
+  allow_stopping_for_update = true
 }
 
 resource "yandex_vpc_network" "ls-lab-2-network" {
@@ -43,26 +44,3 @@ resource "yandex_vpc_subnet" "ls-lab-2-subnet" {
   network_id     = yandex_vpc_network.ls-lab-2-network.id
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
-
-/*
-
-resource "yandex_vpc_network" "network-1" {
-  name = "network1"
-}
-
-resource "yandex_vpc_subnet" "subnet-1" {
-  name           = "subnet1"
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.network-1.id
-  v4_cidr_blocks = ["192.168.10.0/24"]
-}
-
-output "internal_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
-}
-
-output "external_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
-}
-
-*/
